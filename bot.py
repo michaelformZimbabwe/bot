7c37b2a3846a6f8e341ae5ccbf653dd30601a89a
michaelformZimbabwe <51244155+michaelformZimbabwe@users.noreply.github.com>
2019-06-02 16:53:30 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import os
 import telebot
 from telebot.types import Message
-TOKEN= '698380037:AAG2JYt_oD7Cwbh2JtMEaavGQGb_yY-MPRw'
+TOKEN= 'add after'
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands='start')
