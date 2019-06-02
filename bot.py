2e7cc6be0e3b53e474ac964480a680778fba6e52
michaelformZimbabwe <51244155+michaelformZimbabwe@users.noreply.github.com>
2019-06-02 16:37:48 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,10 +1,9 @@
+import os
 import telebot
-from telebot import apihelper
+from telebot.types import Message
 TOKEN= '698380037:AAG2JYt_oD7Cwbh2JtMEaavGQGb_yY-MPRw'
-apihelper.proxy = {'https: socks4://198.50.214.17:6874'}
 bot = telebot.TeleBot(TOKEN)
-message = 'Привет мы любим Китай'
 
 @bot.message_handler(commands='start')
 def send_welcome(message):
-    bot.send_message(message)
+    bot.send_message(message,'Привет')
